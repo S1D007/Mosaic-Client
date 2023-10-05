cf952e5cd729e278015fed12ecee41adda04a789
S1D007 <s1d.official011001011@gmail.com>
2023-10-05 12:35:46 +0530

    added URL

--- src/App.tsx
+++ src/App.tsx
@@ -197,7 +197,7 @@ function App() {
                 setIsSending(true);
                 console.log("data", formData);
                 const response = await axios.post(
-                  "http://gokapture-digital-mosaic-backend-env.eba-edyt79ku.ap-south-1.elasticbeanstalk.com/process-my-image",
+                  "https://gokapture-digital-mosaic-backend-env.eba-edyt79ku.ap-south-1.elasticbeanstalk.com/process-my-image",
                   formData,
                 );
                 setIsSending(true);
